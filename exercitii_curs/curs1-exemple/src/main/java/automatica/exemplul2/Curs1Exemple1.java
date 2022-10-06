0c4b85121a4c0a1a643903a3beb81c1e9d556be5
Mihai <mihai.hulea@aut.utcluj.ro>
2022-10-06 20:33:45 +0300

    Exemple curs 1.

---
+++ exercitii_curs/curs1-exemple/src/main/java/automatica/exemplul2/Curs1Exemple1.java
@@ -0,0 +1,12 @@
+
+package automatica.exemplul2;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Curs1Exemple1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! 2");
+    }
+}
---
+++ exercitii_curs/curs1-exemple/src/main/java/automatica/ssatr/curs1/exemple/Curs1Exemple1.java
@@ -0,0 +1,19 @@
+
+
+package automatica.ssatr.curs1.exemple;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Curs1Exemple1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! 1");
+//        try {
+//            
+//            Thread.sleep(100000);
+//        } catch (InterruptedException ex) {
+//            Logger.getLogger(Curs1Exemple1.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+    }
+}
